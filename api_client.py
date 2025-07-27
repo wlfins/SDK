feabcfe57be12242b50b10fbca3b9f50bbe17a40
wlfins <wlfins@proton.me>
2025-07-27 12:35:20 +0800

    update base URL

--- api_client.py
+++ api_client.py
@@ -1,7 +1,7 @@
 import requests
 
 class WLFIApiClient:
-    def __init__(self, base_url="http://localhost:3001"):
+    def __init__(self, base_url="https://metadata.wlfins.domains/"):
         self.base_url = base_url
 
     def get_image(self, token_id: str):
